ee143f4cfd1372b9e08ea6e790e27e2eabdf6e09
angelfan <angelfan1314@163.com>
2015-05-24 15:11:50 +0800

    俱乐部前台画面

---
+++ 俱乐部活动预约系统_files/mycommon.js
@@ -0,0 +1,106 @@
+﻿$(function(){ 
+    var thisUrl = window.location.pathname
+    var spUrl = c = thisUrl.split('/');
+    var controller = spUrl[1];
+    var method = spUrl[2];
+    if (controller == 'UserZone') {     //个人中心
+        var bread = "个人中心"
+        $("#bread").html(bread)
+        $("#activity").removeClass("active open")
+        $("#activity").siblings().removeClass("active open")
+    } else if (controller == "User") {  //用户管理
+        if (method == "Home") {
+            $("#home").addClass("active")
+            $("#home").siblings().removeClass("active open")
+        }
+        else if (method == "Create") {
+            var bread = "用户管理"
+            $("#bread").html(bread)
+            $("#user").addClass("active open")
+            $("#user").siblings().removeClass("active open")
+            $("#user").find("li").eq(1).addClass("active")
+        }
+        else if (method == "Index") {
+            var bread = "用户管理"
+            $("#bread").html(bread)
+            $("#user").addClass("active open")
+            $("#user").siblings().removeClass("active open")
+            $("#user").find("li").eq(0).addClass("active")
+        }
+        else if (method == "Input") {
+            var bread = "用户管理"
+            $("#bread").html(bread)
+            $("#user").addClass("active open")
+            $("#user").siblings().removeClass("active open")
+            $("#user").find("li").eq(2).addClass("active")
+        } else {
+            var bread = "用户管理"
+            $("#bread").html(bread)
+            $("#user").addClass("active open")
+            $("#user").siblings().removeClass("active open")
+        }
+        
+    } else if (controller == "Club") { //俱乐部管理
+        var bread = "俱乐部管理"
+        $("#bread").html(bread)
+        $("#club").addClass("active open")
+        $("#club").siblings().removeClass("active open")
+        if (method == "Create") {
+            $("#club").find("li").eq(1).addClass("active") //创建俱乐部
+        }
+        if (method == "Index") {
+            $("#club").find("li").eq(0).addClass("active") //俱乐部一览
+        }  
+    } else if (controller == "Activity") {  //活动管理
+        var bread = "活动管理"
+        $("#bread").html(bread)
+        $("#activity").addClass("active open")
+        $("#activity").siblings().removeClass("active open")
+        if (method == "Create") {
+            $("#activity").find("li").eq(1).addClass("active")//活动创建
+        }
+        if (method == "Index") {
+            $("#activity").find("li").eq(0).addClass("active")//活动一览
+        }    
+    } else if (controller == "UserActivity") {  //活动报名
+        var bread = "活动报名"
+        $("#user-activity").addClass("active")
+        $("#user-activity").siblings().removeClass("active open")
+    } else if (controller == "Statistics") { //数据统计
+        var bread = "数据统计"
+        $("#bread").html(bread)
+        $("#statistic").addClass("active open")
+        $("#statistic").siblings().removeClass("active open")
+    }
+    //删除俱乐部申请信息
+    $("#delete-user-examines").click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            dataType: "json",
+            url: $(this).attr("href"),
+            success: function (ret) {
+                return;
+            },
+            error: function () {
+                return;
+            }
+        });
+    });//end delete-user-examines
+})
+//错误信息显示
+function promptMessage(text, flag) {
+    var m = $("#div-alert-message");
+    var s = "";
+    if (flag) {
+        //成功
+        s = "<div class='alert alert-block alert-success'>"
+        + "<button type='button' class='close' data-dismiss='alert'><i class='icon-remove'></i></button>"
+        + "<span><i class='icon-ok green'></i>" + text + "</span></div>";
+    } else {
+        s = "<div class='alert alert-block alert-error'>"
+        + "<button type='button' class='close' data-dismiss='alert'><i class='icon-remove'></i></button>"
+        + "<span><i class='icon-remove red'></i>" + text + "</span></div>";
+    }
+    m.html(s);
+}
---
+++ 俱乐部活动预约系统_files/user.js
@@ -0,0 +1,186 @@
+﻿function bodyLoad() {
+    pageInit();
+}
+
+function pageInit() {
+    //user/create
+    dateInit();
+    //user/index
+    resetPassword();//重置密码
+    deleteUser(); //删除用户
+    getUserPowers();//获取用户权限
+    setUserPowers();//设置用户权限
+    tableInit();//table初始化
+    valMaxClub();//权限验证初始化
+}
+
+//重置密码
+function resetPassword() {
+    $("#table_report").on("click", ".reset-password",function (e) {
+        e.preventDefault();
+        $.ajax({
+            type:"post",
+            dataType: "json",
+            url: $(this).attr("href"),
+            data: {"id":parseInt($(this).attr("value"))},
+            success: function (ret) {
+                switch (ret) {
+                    case 200:
+                        promptMessage("重置密码成功", true);
+                        break;
+                    case 202:
+                        promptMessage("用户信息不完整，请补充完整后再进行操作", false);
+                        break;
+                    case 204:
+                        promptMessage("没有找到该用户", false);
+                        break;
+                    default:
+                        promptMessage("重置密码失败", false);
+                        break;
+                }
+            },
+            error:function(){
+                promptMessage("重置密码失败", false);
+            }   
+        });
+    });
+}
+//删除用户
+function deleteUser() {
+    $("#table_report").on("click", ".delete-user", function (e) {
+        e.preventDefault();
+        var con = confirm("确认删除？");
+        if (con == true) {
+            $.ajax({
+                type: "post",
+                dataType: "html",
+                url: $(this).attr("href"),
+                data: { "id": parseInt($(this).attr("value")) },
+                success: function (ret) {
+                    if (ret == undefined) {
+                        promptMessage("删除失败", false);
+                    } else {
+                        $("#div-table-user").html(ret);
+                        promptMessage("删除成功", true);
+                        pageInit();//初始化
+                    }
+                },
+                error: function () {
+                    promptMessage("删除失败", false);
+                }
+            });
+        }
+    });
+}
+//获取权限
+function getUserPowers() {
+    $("#table_report").on("click", ".get_user_power", function (e) {
+        var id = $(this).attr("value");
+        $.ajax({
+            type: "post",
+            dataType: "html",
+            url: $(this).attr("ahref"),
+            data: { "id": parseInt(id) },
+            success: function (ret) {
+                if (ret == undefined) {
+                    promptMessage("获取权限失败", false);
+                } else {
+                    $("#myModal").find(".controls").html(ret);
+                    $("#mymodel-submit").attr("value", id);
+                    valMaxClub();
+                }
+            },
+            error: function () {
+                promptMessage("获取权限失败", false);
+            }
+        });
+    });
+}
+//设置权限
+function setUserPowers() {
+    $("#myModal").on("click", ".mymodel-submit", function (e) {
+        var str = "";
+        $("#club-table>tbody>tr").each(function () {
+            var s1 = $(this).find(".join");
+            var s2 = $(this).find(".role");
+            str += s1.attr("name") + "=" + s1.val() + "&";
+            str += s2.attr("name") + "=" + s2.val() + "&";
+        });
+        $.ajax({
+            type: "post",
+            dataType: "json",
+            url: $(this).attr("href"),
+            data: {"obj":str,"id": parseInt($(this).attr("value"))},
+            success: function (ret) {
+                switch (ret) {
+                    case 200:
+                        promptMessage("权限设置成功", true);
+                        break;
+                    case 202:
+                        promptMessage("用户信息不完整，请补充完整后再进行操作", false);
+                        break;
+                    case 204:
+                        promptMessage("没有找到该用户", false);
+                        break;
+                    default:
+                        promptMessage("权限设置失败", false);
+                        break;
+                }
+            },
+            error: function () {
+                promptMessage("权限设置失败", false);
+            }
+        });
+    });
+}
+
+//俱乐部选择数
+function valMaxClub() {
+    var count = 0;
+    $(".join").each(function (e) {
+        if (parseInt($(this).val()) == 1) {
+            count++;
+        }
+    });
+    if (count == 1) {
+        $(".join").each(function (e) {
+            $(this).removeAttr("disabled");
+        });
+    }
+    if (count >= 2) {
+        $(".join").each(function (e) {
+            if (parseInt($(this).val()) == 0) {
+                $(this).attr("disabled", "disabled");
+            }
+        });
+    }
+}
+
+function tableInit(){
+    $('#table_report').dataTable({
+        "aoColumns": [
+            { "bSortable": false },
+            null, null, null, null, null, null,
+            { "bSortable": false }
+        ]
+    });
+    $('[data-rel=tooltip]').tooltip();
+}
+
+function dateInit() {
+    if ($("#userFrom").length > 0) {
+        $('#datetimepicker').datetimepicker({
+            language: 'zh-CN',
+            autoclose: true,
+            todayHighlight: true,
+            startView: "year",
+            minView: 'month',
+            pickerPosition: "bottom-left",
+            todayBtn: true
+        });
+    }
+}
+function show_box(id) {
+    $('.widget-box.visible').removeClass('visible');
+    $('#' + id).addClass('visible');
+}
